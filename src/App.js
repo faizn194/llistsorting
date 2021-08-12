0706e990715b94c58e872bd899e9f4f9aa7fd0b9
faizn194 <fazanmuzamal89@gmail.com>
2021-08-11 20:27:58 -0700

    listsorting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTable, useSortBy } from 'react-table'
 
 function App() {
+  const data = React.useMemo(
+    () => [
+      {id: 1, name: 'Faizan',age: 22,DOB: 1996},
+      {id: 2,name: 'Ali',age: 19,DOB: 2001},
+      {id: 3,name: 'Umer',age: 53,DOB: 1999},
+      {id: 4,name: 'Ahsan',age: 25,DOB: 2010},
+      {id: 5,name: 'Bilal',age: 15,DOB: 2005},
+    ],
+    []
+  )
+
+  const columns = React.useMemo(
+    () => [
+
+      {
+        Header: 'ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'age',
+        accessor: 'age',
+      },
+      {
+        Header: 'Date of Birth',
+        accessor: 'DOB',
+      },
+    ],
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data },
+    useSortBy)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table {...getTableProps()} >
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th
+                {...column.getHeaderProps(column.getSortByToggleProps())} >
+                {column.render('Header')}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? '' : '') : ''}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default App;
